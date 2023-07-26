f265cd7c210e48281716373f6266964d7ec67994
HEMANTH M REDDY <110677650+hemanthMRE@users.noreply.github.com>
2023-07-26 09:19:36 +0530

    RecursionBasics.java

--- RecursionBasics.java
+++ RecursionBasics.java
@@ -58,8 +58,58 @@ public class RecursionBasics{
         }
         return firstoccurence(arr, i+1, key);
     }
-public static void main(String[] args) {
-    int arr[] = {1,2,3,4,5};
-    System.out.println(firstoccurence(arr, 0, 5));
-}    
+    public static int lastOccurence(int arr[], int key, int i){
+    if (i == arr. length) {
+        return -1;
+    }
+    int isFound = lastOccurence(arr, key, i+1);
+    if(isFound==-1 && arr[i]==key){
+        return i;
+    }
+    return isFound;
+}
+    public static int power(int x, int n){
+        if(n==0){
+            return 1;
+        }
+        return x * power(x, n-1);
+    }
+    public static int optmizedPower(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n % 2 == 0) {
+            int temp = power(x, n / 2);
+            return temp * temp;
+        } else {
+            return x * power(x, n - 1);
+        }
+    }
+    public static int tileingProblem(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        //vertical
+       int fm1 = tileingProblem(n-1);
+       //horizontal
+       int fm2 = tileingProblem(n-2);
+        int tnw = fm1 + fm2;
+        return tnw;
+    }
+    public static void towerOfHanoi(int n, char src, char dest, char help) {
+        if (n == 1) {
+            System.out.println("Move disk " + n + " from " + src + " to " + dest);
+            return;
+        }
+    
+        towerOfHanoi(n - 1, src, help, dest);
+        System.out.println("Move disk " + n + " from " + src + " to " + dest);
+        towerOfHanoi(n - 1, help, dest, src);
+    }
+    
+    public static void main(String[] args) {
+          // int arr[] = {1,2,3,4,5,7,8,5,7};
+        int n = 3;
+        towerOfHanoi(n, 'A', 'C', 'B');
+    }  
 }
